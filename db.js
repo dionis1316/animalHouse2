d55249e316ccf6f2ebb50a908cee73197cead3be
Dionis Campines <info@dcsolution.net>
2019-08-06 09:49:55 -0500

    conf archivo db

--- db.js
+++ db.js
@@ -1,8 +1,14 @@
 var admin = require('firebase-admin');
 
 admin.initializeApp({
-    credential: admin.credential.cert("AIzaSyDqlQVAjrGbJE3xd68ZOzqhNVRoIEjmixE"),
-    databaseURL: "https://petsstore-aaa1f.firebaseio.com"
+    apiKey: "AIzaSyDqlQVAjrGbJE3xd68ZOzqhNVRoIEjmixE",
+    authDomain: "petsstore-aaa1f.firebaseapp.com",
+    databaseURL: "https://petsstore-aaa1f.firebaseio.com",
+    projectId: "petsstore-aaa1f",
+    storageBucket: "petsstore-aaa1f.appspot.com",
+    messagingSenderId: "206475584151",
+    appId: "1:206475584151:web:3ec41109223a4459"
+
 });
 var db = admin.database();
 
