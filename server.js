93996d3ecb12927464a5dc5f9c59d29c2198b7d3
iavega <vegaiam11@gmail.com>
2019-08-06 19:04:00 -0500

    index terminado

--- server.js
+++ server.js
@@ -17,15 +17,37 @@ app.use('/js',express.static(path.join(__dirname, 'public/js')));
 app.get('/', (req, res) => {
     res.render('index.pug');
 });
-app.get('/productos', (req, res) => {
-    res.json([{name:'Juguete 001' ,price:3},
-    {name:'Juguete 002' ,price:2},
-    {name:'Juguete 003' ,price:4},
-    {name:'Juguete 004' ,price:2},
-    {name:'Juguete 005' ,price:3},
-    {name:'Juguete 006' ,price:5},
-    {name:'Juguete 007' ,price:3},
-    {name:'Juguete 008' ,price:4}]);
+app.get('/ProductosFech', (req, res) => {
+    res.json([
+        {name:'Juguete 001' ,price:3},
+        {name:'Juguete 002' ,price:2},
+        {name:'Juguete 003' ,price:4},
+        {name:'Juguete 004' ,price:2},
+        {name:'Juguete 005' ,price:3},
+        {name:'Juguete 006' ,price:5},
+        {name:'Juguete 007' ,price:3},
+        {name:'Juguete 008' ,price:4}
+    ]);
+});
+/*app.get('/AdopcionFech', (req, res) => {
+  res.json([
+    {name:'Felipita' ,donacion:1},
+    {name:'Lolita' ,donacion:1},
+    {name:'Pocha' ,donacion:1},
+    {name:'Pili' ,donacion:1}
+  ]);
+});*/
+app.get('/AlimentosFech', (req, res) => {
+  res.json([
+    {name:'Alimento #1' ,price:3},
+    {name:'Alimento #2' ,price:4},
+    {name:'Alimento #3' ,price:2},
+    {name:'Alimento #4' ,price:5},
+    {name:'Alimento #5' ,price:7},
+    {name:'Alimento #6' ,price:8},
+    {name:'Alimento #7' ,price:2},
+    {name:'Alimento #8' ,price:3}
+  ]);
 });
 app.get('/carrito', (req, res) => {
   res.render('cart.pug');
